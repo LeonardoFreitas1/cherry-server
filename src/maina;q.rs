ee2c6436732904ba812369d3ecc98061b0aa2ce3
Leonardo Freitas <leonardo.paulino.ps@ojc.com.br>
2023-01-06 13:27:09 -0300

    feature: initial setup

--- src/maina;q.rs
+++ src/maina;q.rs
@@ -2,18 +2,25 @@ mod handlers;
 use std::io::Result;
 use actix_web::{ App, HttpServer, middleware::Logger};
 use handlers::user_handler::use_scope;
+use sqlx::{postgres::PgPoolOptions};
+use dotenv::dotenv;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    std::env::set_var("RUST_LOG", "debug");
-    std::env::set_var("RUST_BACKTRACE", "1");
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("expected database url");
     env_logger::init();
 
+    let _db = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await;
+    
     HttpServer::new(|| {
         App::new()
         .wrap(Logger::default())
         .service(use_scope())
     }).bind(("127.0.0.1", 9090))?
-    .run()
-        .await
+    .run().await
 }
